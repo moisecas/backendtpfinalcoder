602fbdb452ce80d20ff20340195d30fa702ff5a8
Your Name <moisesjuniorcas20@gmail.com>
2022-09-11 18:20:15 -0500

    agregando ruta

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(epxress.json())
 
-
+// routes
+app.use('/api/users', require('./app/routes')) // http://localhost:3000/api/users 
 
 dbConnect()
 app.listen(PORT, () => {
     console.log('API ok corriendo en ', PORT)
 })
+
