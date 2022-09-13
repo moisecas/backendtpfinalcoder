b3b061f032ce2cd02add1d404d8e99d24c4e7721
Your Name <moisesjuniorcas20@gmail.com>
2022-09-13 09:07:50 -0500

    se agrega id

--- app/models/users.js
+++ app/models/users.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 
 const UsersSchema = new mongoose.Schema({
+    id: {type: Number, 
+        required: true
+    },
     name: {
         type: String,
     },
